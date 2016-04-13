0142e8613b07655e8bec5be7f8e9c259ae8b0782
Nikita Tarasov <nikita@mygento.ru>
2016-04-13 15:44:55 +0300

    small fixes

--- app/code/community/Mygento/Geoip/Block/Version.php
+++ app/code/community/Mygento/Geoip/Block/Version.php
@@ -3,10 +3,6 @@
 class Mygento_Geoip_Block_Version extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface
 {
 
-    private $_name = 'cdn';
-    private $_full = 'Mygento_Geoip';
-    private $_url = '/modules/geoip.html';
-
     /**
      *
      * @param Varien_Data_Form_Element_Abstract $element
@@ -16,16 +12,8 @@ class Mygento_Geoip_Block_Version extends Mage_Adminhtml_Block_Abstract implemen
      */
     public function render(Varien_Data_Form_Element_Abstract $element)
     {
-        if ($curl = curl_init()) {
-            curl_setopt($curl, CURLOPT_URL, 'http://www.mygento.ru/extension/module/index/name/' . $this->_name . '/version/' . Mage::getConfig()->getNode('modules/' . $this->_full . '/version'));
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $data = json_decode(curl_exec($curl));
-            curl_close($curl);
-        }
-        $info = '<fieldset class="config' . (!$data->result ? ' success-msg' : ' error-msg') . '" style="padding-left:30px;">' .
-            '<img src="//www.mygento.ru/media/favicon/default/favicon.png" width="16" height="16" />' .
-            $this->__('Mygento GeoIP version: %s', Mage::getConfig()->getNode('modules/' . $this->_full . '/version'));
-        $info.='<a style="float:right" target="_blank" href="' . $this->_url . '">' . ($data->result ? $this->__('Check for update') . ' [' . $data->version . ']' : $this->__('Module page')) . '</a></fieldset>';
+        $info = '<fieldset class="config success-msg" style="padding-left:30px;"><a target="_blank" href="https://www.mygento.ru/"><img src="//www.mygento.ru/media/favicon/default/favicon.png" width="16" height="16" />' . $this->__('Magento Development') . '</a>';
+        $info.='<a style="float:right" target="_blank" href="https://github.com/mygento/geoip">' . $this->__('Module on Github') . '</a></fieldset>';
         return $info;
     }
 }
